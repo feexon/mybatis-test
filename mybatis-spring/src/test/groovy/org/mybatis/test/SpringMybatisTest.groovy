440448795ed48c4bbb192ebce43611f5768f33c5
xiaoliang <feexon@qq.com>
2014-06-11 11:55:26 +0800

    test managed transaction by spring

--- mybatis-spring/src/test/groovy/org/mybatis/test/SpringMybatisTest.groovy
+++ mybatis-spring/src/test/groovy/org/mybatis/test/SpringMybatisTest.groovy
@@ -3,22 +3,32 @@ package org.mybatis.test
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mybatis.mapper.BlogMapper
+import org.mybatis.pojos.Blog
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.transaction.TransactionConfiguration
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * @author Administrator
  * @version 1.0 2014/6/11,10:07
  */
-@RunWith(SpringJUnit4ClassRunner)
+
 @ContextConfiguration("classpath:mybatis-spring.xml")
-class SpringMybatisTest {
+class SpringMybatisTest extends AbstractTransactionalJUnit4SpringContextTests{
     @Autowired
     private BlogMapper blogs;
 
     @Test
     public void selectBlog() throws Exception {
         assert blogs.selectBlog(1).title == "MyBatis";
+    }
+
+    @Test
+    public void insertBlog() throws Exception {
+        blogs.createNewBlog(new Blog(id: 4, title: 'mybatis-spring'));
+        assert blogs.selectBlog(4).title == 'mybatis-spring';
     }
 }
--- mybatis-test-commons/src/test/groovy/org/mybatis/mapper/BlogMapper.groovy
+++ mybatis-test-commons/src/test/groovy/org/mybatis/mapper/BlogMapper.groovy
@@ -1,12 +1,16 @@
 package org.mybatis.mapper
 
 import org.mybatis.pojos.Blog
+import org.springframework.transaction.annotation.Propagation
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * @author Administrator
  * @version 1.0 2014/6/10,16:29
  */
+@Transactional(propagation = Propagation.REQUIRED)
 public interface BlogMapper {
+    @Transactional(readOnly = true)
     Blog selectBlog(int blogId);
 
     int createNewBlog(Blog blog);
